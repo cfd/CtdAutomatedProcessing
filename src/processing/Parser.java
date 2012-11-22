eb3680e48f5a63549fd99cff0c9fe5885f0360c5
Brendan Russo <brendanrusso27@gmail.com>
2012-11-22 11:20:03 +1000

    added changes while Chris was being screwy

--- src/processing/Parser.java
+++ src/processing/Parser.java
@@ -1,6 +1,8 @@
 package processing;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.LinkedHashMap;
 import util.ConnectDB;
@@ -24,15 +26,46 @@ public class Parser {
 
 	private void populateSensorsMap() {
 		ConnectDB db = new ConnectDB();
-		//call database
-		//make objects of SensorInfo
-		//put in map
-		//sensorID as key, object created as value
+		con = db.getDdConnection();
+		try {
+			statement = con.createStatement();
+			
+			ResultSet results = getAllAttributes();
+			while(results.next()){
+				int sensorID = results.getInt("sensor_ID");
+				int calcID = results.getInt("calc_ID");
+				int unitID = results.getInt("unit_ID");
+				int ordinal = results.getInt("ordinal");
+				String name = results.getString("full_name");
+				sensorsMap.put(sensorID, new SensorInfo(unitID, sensorID, calcID, ordinal, name));
+			}
+			
+			System.out.println("Sensor Map");
+			System.out.println(sensorsMap.toString());
+			System.out.println(sensorsMap.get(3).toString());
+			con.close();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
 		
 	}
-
-
-
+	
+	/**
+	 * Returns a ResultSet of all records from the sensor_info table
+	 * @return ResultSet
+	 */
+	public ResultSet getAllAttributes(){
+		try{
+			String sql = "SELECT * FROM sensor_info";
+			ResultSet rs = statement.executeQuery(sql);
+			return rs;
+		}catch(SQLException e){
+			e.printStackTrace();
+			return null;
+		}
+	}
 
 	/**
 	 * @param args
