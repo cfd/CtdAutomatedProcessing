a354f849cdfb754d039a6122b007882694639288
Brendan Russo <brendanrusso27@gmail.com>
2012-11-26 15:02:32 +1000

    Updated the writer file so userPoly name is a member field. Also updated the interface to have write calc array have userPoly passed in so it can be added where required

--- src/strategy/DatCnvWriter.java
+++ src/strategy/DatCnvWriter.java
@@ -1,24 +1,25 @@
 package strategy;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.jdom2.Attribute;
-import org.jdom2.Content;
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.JDOMException;
 import org.jdom2.input.SAXBuilder;
+import org.jdom2.output.Format;
+import org.jdom2.output.XMLOutputter;
 
 import model.SensorInfo;
 
 public class DatCnvWriter implements IPsaWriter {
-	final boolean DEBUG = true;
+	final boolean DEBUG = false;
 	ArrayList<SensorInfo> sensors;
-	Document readDoc;
-	ArrayList<Element> upperSection = new ArrayList<>();
+	Document doc;
 
 	@Override
 	public void setup(ArrayList<SensorInfo> orderedSensors) {
@@ -31,11 +32,10 @@ public class DatCnvWriter implements IPsaWriter {
 	@Override
 	public void readTemplate() throws JDOMException, IOException {
 		SAXBuilder builder = new SAXBuilder();
-
-		readDoc = builder.build(new File("psa_templates/DataCnvTemplate.xml"));
-
+		doc = builder.build(new File("psa_templates/DataCnvTemplate.xml"));
+		
 		if (DEBUG) {
-			System.out.println("I Read The FIle");
+			System.out.println("I Read The File");
 		}
 		// TODO Auto-generated method stub
 
@@ -46,36 +46,37 @@ public class DatCnvWriter implements IPsaWriter {
 		if (DEBUG) {
 			System.out.println("Writter upper section");
 		}
-		Element root = readDoc.getRootElement();
-		List<Element> allSection = root.getChildren();
-		for (Element element : allSection) {
-			if (element.getName().equals("CalcArray")) {
-				break;
-			}
-			upperSection.add(element);
-			if (DEBUG) {
-				System.out.println(element.getName());
-				System.out.println(element.getAttributes());
-				System.out.println();
-			}
-
-		}
-		// TODO Auto-generated method stub
 	}
 
 	@Override
-	public void writeCalcArray() {
-		Element calcArray = new Element("CalcArray");
-		ArrayList<Element> CalcArrayItem = new ArrayList<>();
-		Element timeElapsed = new Element("CalcArrayItem");
-		timeElapsed.setAttribute("index","0");
-		timeElapsed.setAttribute("CalcID", "84");
-		if (DEBUG){
-		System.out.println(timeElapsed.getAttributes());
+	public void writeCalcArray(String userPoly) {
+		Element root = doc.getRootElement();
+		Element calcArray = root.getChild("CalcArray");
+
+		// Adds size to the calc array
+		calcArray.setAttribute("Size", "" + (sensors.size() + 2));
+
+		// Creates the Time Elapsed and Scan Count array items
+		Element timeElapsed = calcArrayItemWritter(0, 84, 52, 0,
+				"Time, Elapsed [seconds]");
+		Element scanCount = calcArrayItemWritter(1, 72, -1, 0, "Scan Count");
+
+		// Adds the Time Elapsed and Scan Count items
+		calcArray.addContent(timeElapsed);
+		calcArray.addContent(scanCount);
+
+		int index = 2;
+		for (SensorInfo sensor : sensors) {
+			calcArray.addContent(calcArrayItemWritter(index++,
+					sensor.getCalcID(), sensor.getUnitID(),
+					sensor.getOrdinal(), sensor.getFullname()));
 		}
-		//Content timeElapsed;
-		
-		for(SensorInfo sensor : sensors){
+
+		if (DEBUG) {
+			System.out.println(sensors.size() + 2);
+		}
+
+		for (SensorInfo sensor : sensors) {
 			System.out.println(sensor.getFullname());
 		}
 		// TODO Auto-generated method stub
@@ -89,9 +90,61 @@ public class DatCnvWriter implements IPsaWriter {
 	}
 
 	@Override
-	public void writeToNewPsaFile() {
-		// TODO Auto-generated method stub
+	public void writeToNewPsaFile() throws FileNotFoundException, IOException {
 
+		XMLOutputter xmlOutput = new XMLOutputter(Format.getPrettyFormat());
+		;
+		xmlOutput.output(doc, new FileOutputStream(new File(
+				"output/DatCnvIMOS.psa")));
+		System.out.println("Wrote to file");
+
+	}
+
+	private Element calcArrayItemWritter(int index, int calcID, int unitID,
+			int ordinal, String fullname) {
+		//Sets up calc and calcArray Elements
+		Element calcArrayItem = new Element("CalcArrayItem");
+		Element calc = new Element("Calc");
+
+		// Does the full name and adds it
+		Element name = new Element("FullName");
+		name.setAttribute("value", fullname);
+		calc.addContent(name);
+		
+		//Sets up the WindowSize
+		Element windowSize = new Element("WindowSize");
+		windowSize.setAttribute("value", "2.000000");
+		
+		//Sets up the ApplyHysteresisCorrection
+		Element hysteresis = new Element("ApplyHysteresisCorrection");
+		hysteresis.setAttribute("value", "1");
+		
+		//Sets up the ApplyTauCorrection
+		Element tau = new Element("ApplyTauCorrection");
+		tau.setAttribute("value", "1");
+		
+		//Checks if any extra detail is needed
+		switch(fullname){
+		case "Descent Rate [m/s]":
+			calc.addContent(windowSize);
+			break;
+		case "Oxygen, SBE 43 [ml/l]":
+			calc.addContent(windowSize);
+			calc.addContent(hysteresis);
+			calc.addContent(tau);
+			break;
+		}
+		
+		// Sets the calc
+		calc.setAttribute("UnitID", "" + unitID);
+		calc.setAttribute("Ordinal", "" + ordinal);
+		
+
+		// Sets the calcArrayItem
+		calcArrayItem.setAttribute("index", "" + index);
+		calcArrayItem.setAttribute("CalcID", "" + calcID);
+		calcArrayItem.addContent(calc);
+		return calcArrayItem;
 	}
 
 }
