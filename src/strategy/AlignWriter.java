bba7f43afe85fd9b3ee2c4baaa3e7b25c095c73e
Brendan Russo <brendanrusso27@gmail.com>
2012-12-06 10:08:03 +1000

    Changed the Align Writer to not have Pressure just comment out the 57th line of AlignWriter.java to include Pressure XD

--- src/strategy/AlignWriter.java
+++ src/strategy/AlignWriter.java
@@ -17,15 +17,16 @@ import org.jdom2.output.XMLOutputter;
 import org.jdom2.input.SAXBuilder;
 
 public class AlignWriter implements IPsaWriter {
-	
+
 	XMLOutputter xmlOutput;
-	
+
 	public AlignWriter() {
 		xmlOutput = new XMLOutputter(Format.getPrettyFormat());
 	}
-	
+
 	ArrayList<SensorInfo> sensors;
-	Document doc;	
+	Document doc;
+
 	@Override
 	public void setup(ArrayList<SensorInfo> orderedSensors) {
 		sensors = orderedSensors;
@@ -35,21 +36,25 @@ public class AlignWriter implements IPsaWriter {
 	}
 
 	@Override
-	public void readTemplate(String psaTemplate) throws JDOMException, IOException {
+	public void readTemplate(String psaTemplate) throws JDOMException,
+			IOException {
 		// TODO Auto-generated method stub
-		SAXBuilder builder =  new SAXBuilder();
-		doc =  builder.build(new File(psaTemplate + "\\AlignTemplate.xml"));
+		SAXBuilder builder = new SAXBuilder();
+		doc = builder.build(new File(psaTemplate + "\\AlignTemplate.xml"));
 	}
 
 	@Override
 	public void writeUpperSection(String workingDirectory, String instrumentPath) {
 		// TODO Auto-generated method stub
-		////UPPER SECTION NOT COMPLETED YET!!!/////
+		// //UPPER SECTION NOT COMPLETED YET!!!/////
 		Element root = doc.getRootElement();
 		Element inputDir = root.getChild("InputDir");
 		inputDir.setAttribute("value", workingDirectory + "batch");
 		Element outputDir = root.getChild("OutputDir");
 		outputDir.setAttribute("value", workingDirectory + "batch");
+		
+		//To remove Pressure from align
+		sensors.remove(0);
 	}
 
 	@Override
@@ -59,7 +64,7 @@ public class AlignWriter implements IPsaWriter {
 		Element rootElement = doc.getRootElement();
 		Element calcArray = rootElement.getChild("CalcArray");
 		calcArray.setAttribute("Size", sensors.size() + "");
-		
+
 		for (SensorInfo info : sensors) {
 
 			if (info != null) {
@@ -79,9 +84,10 @@ public class AlignWriter implements IPsaWriter {
 				calcArray.addContent(calcArrayItem);
 				calcArrayItem.addContent(calc);
 				calc.addContent(fullname);
-				
+
 				if (info.getFullName().startsWith("Upoly")) {
-					fullname.setAttribute("value", "Upoly 0, " + info.getFullName() + ", "  + userPoly);
+					fullname.setAttribute("value",
+							"Upoly 0, " + info.getFullName() + ", " + userPoly);
 					Element calcName = new Element("CalcName");
 					calcName.setAttribute("value", "Upoly 0, " + userPoly);
 					calc.addContent(calcName);
@@ -97,12 +103,12 @@ public class AlignWriter implements IPsaWriter {
 					calc.addContent(applyH);
 					calc.addContent(tau);
 				}
-				if (info.getFullName().startsWith("Descent")){
+				if (info.getFullName().startsWith("Descent")) {
 					Element windowSize = new Element("WindowSize");
 					windowSize.setAttribute("value", "2.000000");
 					calc.addContent(windowSize);
 				}
-			count++;
+				count++;
 			}
 		}
 	}
@@ -114,47 +120,50 @@ public class AlignWriter implements IPsaWriter {
 		Element rootElement = doc.getRootElement();
 		Element valArray = rootElement.getChild("ValArray");
 		valArray.setAttribute("size", sensors.size() + "");
-		
+
 		for (SensorInfo info : sensors) {
 
 			if (info != null) {
 
 				Element valArrayItem = new Element("ValArrayItem");
-				
+
 				valArrayItem.setAttribute("index", "" + count1);
-				
-				if (info.getFullName().startsWith("Temperature")){
+
+				if (info.getFullName().startsWith("Temperature")) {
 					valArrayItem.setAttribute("value", "0.500000");
 				} else {
 					valArrayItem.setAttribute("value", "0.000000");
 				}
-				
+
 				valArrayItem.setAttribute("variable_name", info.getFullName());
-				
+
 				int i = info.getFullName().indexOf('[');
-				if (i != -1){
-					valArrayItem.setAttribute("variable_name", info.shortenName(i));
+				if (i != -1) {
+					valArrayItem.setAttribute("variable_name",
+							info.shortenName(i));
 				}
-				
-				if (info.getFullName().startsWith("Upoly")){
-					valArrayItem.setAttribute("variable_name", "User Polynomial");
+
+				if (info.getFullName().startsWith("Upoly")) {
+					valArrayItem.setAttribute("variable_name",
+							"User Polynomial");
 				}
-				
-				if (info.getFullName().startsWith("Frequency")){
+
+				if (info.getFullName().startsWith("Frequency")) {
 					valArrayItem.setAttribute("variable_name", "Frequency");
 				}
-				
+
 				valArray.addContent(valArrayItem);
 			}
-		count1++;
+			count1++;
 		}
 	}
 
 	@Override
-	public void writeToNewPsaFile(String newDirName) throws FileNotFoundException, IOException {
+	public void writeToNewPsaFile(String newDirName)
+			throws FileNotFoundException, IOException {
 		// TODO Auto-generated method stub
-		xmlOutput.output(doc, new FileOutputStream(new File(
-				newDirName + "/AlignIMOS.psa")));
+		xmlOutput.output(doc, new FileOutputStream(new File(newDirName
+				+ "/AlignIMOS.psa")));
 		System.out.println("AlignIMOS.psa File Written!");
 	}
 }
