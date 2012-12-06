89d34ad8cfe0e5ce88270d4d271b4fd3a0bd9b0b
Brendan Russo <brendanrusso27@gmail.com>
2012-12-06 15:55:00 +1000

    Changed file name to not have .xmlcon

--- src/strategy/Writer.java
+++ src/strategy/Writer.java
@@ -199,7 +199,7 @@ public class Writer {
 				writers.add(loopEditWriter);
 
 				// Where the psa writes to
-				String outputDirName = DIRECTORY + "\\config\\" + xml.getName();
+				String outputDirName = DIRECTORY + "\\config\\" + xml.getName().substring(0, xml.getName().length() - 7);
 				new File(outputDirName).mkdir();
 
 				// Makes the data stuff
