92b8a492b2d136d5ef69a506f6ddfbb4c34e9205
Brendan Russo <brendanrusso27@gmail.com>
2012-12-13 14:27:52 +1000

    Changed the calibration date and start and end date to work off dates because it's easier to compare them. Can pull instrument ID for a hex file based on the hex's Serial Number and date compared to the Instruments Serial Number and date.

--- src/hex/HexReader.java
+++ src/hex/HexReader.java
@@ -7,6 +7,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.LinkedHashMap;
@@ -16,29 +17,31 @@ import util.ConnectDB;
 
 public class HexReader {
 
-	private final static LinkedHashMap<String,String> months = new LinkedHashMap<String,String>();
+	private final static LinkedHashMap<String, String> months = new LinkedHashMap<String, String>();
 	private static Connection con;
 	private static Statement statement;
-	
+	private static final DateFormat dateFormat = new SimpleDateFormat(
+			"dd/MM/yyyy");
+
 	static {
-		//initalize LinkedHashMap
-		months.put("jan","01");
-		months.put("feb","02");
-		months.put("mar","03");
-		months.put("apr","04");
-		months.put("may","05");
-		months.put("jun","06");
-		months.put("jul","07");
-		months.put("aug","08");
-		months.put("sep","09");
-		months.put("oct","10");
-		months.put("nov","11");
-		months.put("dec","12");
+		// initalize LinkedHashMap
+		months.put("jan", "01");
+		months.put("feb", "02");
+		months.put("mar", "03");
+		months.put("apr", "04");
+		months.put("may", "05");
+		months.put("jun", "06");
+		months.put("jul", "07");
+		months.put("aug", "08");
+		months.put("sep", "09");
+		months.put("oct", "10");
+		months.put("nov", "11");
+		months.put("dec", "12");
 	}
-	
+
 	private static boolean DEBUG = true;
 	private String serialNo;
-	private String calibrationDate;
+	private Date calibrationDate;
 
 	private String path;
 
@@ -65,16 +68,17 @@ public class HexReader {
 						System.out.println(line);
 						System.out.println(serialNo);
 					}
-					
+
 				}
 
 				if (line.startsWith("* System UpLoad Time")) {
-					calibrationDate = formatDate(line.split("=")[1].trim());					
+					calibrationDate = formatDate(line.split("=")[1].trim());
 					if (DEBUG) {
 						System.out.println(line);
-						System.out.println(calibrationDate);
+						System.out.println("Date: "
+								+ calibrationDate.toString());
 					}
-					
+
 				}
 
 			}
@@ -84,108 +88,108 @@ public class HexReader {
 		catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
-		//TODO move somwhere else
-		//initalize Database
-		ConnectDB db = new ConnectDB();
-		con = db.getDdConnection();
-		try{
-			statement = con.createStatement();
-			ResultSet results = getConFile();
-			DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-			
-			while (results.next()){
-				String instrumentID = results.getString("Instrument_ID");
-				String startDate = results.getString("Start_Date");
-				Date start = new Date();
-				String endDate = results.getString("End_Date");
-				if(endDate != null && endDate.toLowerCase().equals("current")){
-					Date date = new Date();
-					System.out.println("I do things: " + dateFormat.format(date));
-				}
-				System.out.println(instrumentID + ", " + startDate + ", " + endDate);
-			}
-			con.close();
-		} catch (SQLException e){
-			System.out.println("I crashed here");
+		try {
+			getConInfo();
+		} catch (ParseException | SQLException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
-	
+
+	private void getConInfo() throws ParseException, SQLException {
+		// initalize Database
+		ConnectDB db = new ConnectDB();
+		con = db.getDdConnection();
+
+		statement = con.createStatement();
+		ResultSet results = getConFile();
+
+		while (results.next()) {
+			String instrumentID = results.getString("Instrument_ID");
+
+			Date startDate = new Date();
+			Date endDate = new Date();
+
+			String stringStartDate = results.getString("Start_Date");
+			String stringEndDate = results.getString("End_Date");
+			
+			//Checks for null values
+			if (stringEndDate == null || stringStartDate == null){
+				continue;
+			}
+			//Checks if there needs to be current date
+			else if(stringEndDate.toLowerCase().equals("current")){
+				startDate = dateFormat.parse(stringStartDate);
+			}else{
+				startDate = dateFormat.parse(stringStartDate);
+				endDate = dateFormat.parse(stringEndDate);
+			}
+			
+			if(DEBUG){
+				System.out.println(instrumentID + ", " + stringStartDate + ", "
+					+ stringEndDate);
+				System.out.printf("Start Date: %s%nEnd Date: %s%n", dateFormat.format(startDate), dateFormat.format(endDate));
+			}
+			
+			if((startDate.before(calibrationDate) || startDate.equals(calibrationDate)) && endDate.after(calibrationDate)){
+				if(DEBUG){
+					System.out.println("InstrumentID is: " + instrumentID);
+				}
+				break;
+			}
+			
+		}
+		con.close();
+	}
+
 	/**
 	 * Formats the date to the same way as it is in the datebase
+	 * 
 	 * @param badDate
 	 * @return
 	 */
-	private String formatDate(String badDate){
+	private Date formatDate(String badDate) {
 		String[] dateSplit = badDate.split(" ");
 		String day = dateSplit[1];
 		String month = months.get(dateSplit[0].toLowerCase());
 		String year = dateSplit[2];
-		
-		if (DEBUG){
-			System.out.printf("Day: %s, Month: %s, Year: %s%n", day, month, year);
+
+		if (DEBUG) {
+			System.out.printf("Day: %s, Month: %s, Year: %s%n", day, month,
+					year);
 		}
-		
-//		//Converts the date from 3 character to 2 number
-//		switch (month.toLowerCase()){
-//		case "jan" :
-//			month = "01";
-//			break;
-//		case "feb" :
-//			month = "02";
-//			break;
-//		case "mar" :
-//			month = "03";
-//			break;
-//		case "apr" :
-//			month = "04";
-//			break;
-//		case "may" :
-//			month = "05";
-//			break;
-//		case "jun" :
-//			month = "06";
-//			break;
-//		case "jul" :
-//			month = "07";
-//			break;
-//		case "aug" :
-//			month = "08";
-//			break;
-//		case "sep" :
-//			month = "09";
-//			break;
-//		case "oct" :
-//			month = "10";
-//			break;
-//		case "nov" :
-//			month = "11";
-//			break;
-//		case "dec" :
-//			month = "12";
-//			break;
-//		}
-		
-		return String.format("%s/%s/%s", day, month, year);
+
+		try {
+			Date date = dateFormat.parse(day + "/" + month + "/" + year);
+			return date;
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+
+		// return String.format("%s/%s/%s", day, month, year);
 	}
-	
-	private ResultSet getConFile(){
-		try{
-			String sql = String.format("SELECT * FROM Instrument_Calibration as IC, Instrument_Details as ID WHERE IC.Serial_No = ID.Serial_no and IC.Serial_No = '%s'", serialNo);
+
+	private ResultSet getConFile() {
+		try {
+			String sql = String
+					.format("SELECT * FROM Instrument_Calibration as IC, Instrument_Details as ID WHERE IC.Serial_No = ID.Serial_no and IC.Serial_No = '%s'",
+							serialNo);
 			ResultSet rs = statement.executeQuery(sql);
 			return rs;
-		}catch(SQLException e){
+		} catch (SQLException e) {
 			e.printStackTrace();
 			return null;
 		}
 	}
-	
-	public String getCalibrationDate(){
+
+	public Date getCalibrationDate() {
 		return calibrationDate;
 	}
-	
-	public String getSerialNo(){
+
+	public String getSerialNo() {
 		return serialNo;
 	}
 
