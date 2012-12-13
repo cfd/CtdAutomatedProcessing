d75aaa1c5b5968e2fe2ff426b575301ab72874ec
akst <aksthomsen@gmail.com>
2012-12-13 11:36:40 +1000

    HexReader class now works as an object and is compatible with different hex files.

--- src/hex/HexReader.java
+++ src/hex/HexReader.java
@@ -5,38 +5,126 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class HexReader {
-	 
-	public static void main(String[] args) {
-	 
-	        // Location of file to read
-	        File file = new File("\\\\pearl\\temp\\adc-jcu2012\\config\\19plus1_4409_20120905\\data\\raw\\GB12071.hex");
-	 
-	        try {
-	 
-	            Scanner scanner = new Scanner(file);
-	            
-	            int count = 1;
+
+	private static boolean DEBUG = true;
+	private String serialNo;
+	private String calibrationDate;
+
+	private String path;
+
+	public HexReader(String path) {
+		this.path = path;
+	}
+
+	public void run() {
+
+		// Location of file to read
+		File file = new File(path);
+
+		try {
+
+			Scanner scanner = new Scanner(file);
+
+			int count = 1;
+
+			while (scanner.hasNextLine() && count++ < 10) {
+				String line = scanner.nextLine();
+				if (line.startsWith("* Temperature")) {
+					serialNo = line.split("=")[1].trim();
+					if (DEBUG) {
+						System.out.println(line);
+						System.out.println(serialNo);
+					}
+					
+				}
+
+				if (line.startsWith("* System UpLoad Time")) {
+					calibrationDate = formatDate(line.split("=")[1].trim());					
+					if (DEBUG) {
+						System.out.println(line);
+						System.out.println(calibrationDate);
+					}
+					
+				}
+
+			}
+			scanner.close();
+		}
+
+		catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
 	
-	            while (scanner.hasNextLine() && count++ < 10) {
-	                String line = scanner.nextLine();
-	                if (line.startsWith("* Temperature")) {
-	                	System.out.println(line);
-	                	int tempSerial = Integer.parseInt(line.substring(line.length()- 4));
-	                	System.out.println(tempSerial);
-	                }
-	                
-	                if (line.startsWith("* System UpLoad Time")) {
-	                	System.out.println(line);
-	                	String uploadDate = line.substring(line.length()- 20);
-	                	System.out.println(uploadDate);
-	                }  
-	                
-	            }
-	            scanner.close();     
-	        }
-	        
-	        catch (FileNotFoundException e) {
-	        		e.printStackTrace();
-	        }
-	 }
+	/**
+	 * Formats the date to the same way as it is in the datebase
+	 * @param badDate
+	 * @return
+	 */
+	private String formatDate(String badDate){
+		String[] dateSplit = badDate.split(" ");
+		String day = dateSplit[1];
+		String month = dateSplit[0];
+		String year = dateSplit[2];
+		
+		if (DEBUG){
+			System.out.printf("Day: %s, Month: %s, Year: %s%n", day, month, year);
+		}
+		
+		//Converts the date from 3 character to 2 number
+		switch (month.toLowerCase()){
+		case "jan" :
+			month = "01";
+			break;
+		case "feb" :
+			month = "02";
+			break;
+		case "mar" :
+			month = "03";
+			break;
+		case "apr" :
+			month = "04";
+			break;
+		case "may" :
+			month = "05";
+			break;
+		case "jun" :
+			month = "06";
+			break;
+		case "jul" :
+			month = "07";
+			break;
+		case "aug" :
+			month = "08";
+			break;
+		case "sep" :
+			month = "09";
+			break;
+		case "oct" :
+			month = "10";
+			break;
+		case "nov" :
+			month = "11";
+			break;
+		case "dec" :
+			month = "12";
+			break;
+		}
+		
+		return String.format("%s/%s/%s", day, month, year);
+	}
+	
+	public String getCalibrationDate(){
+		return calibrationDate;
+	}
+	
+	public String getSerialNo(){
+		return serialNo;
+	}
+
+	public static void main(String args[]) {
+		HexReader reader = new HexReader(
+				"\\\\pearl\\temp\\adc-jcu2012\\config\\19plus1_4409_20120905\\data\\raw\\GB12071.hex");
+		reader.run();
+	}
 }
